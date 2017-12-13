d003bd09ede2c4a07a26383894ede4e9c940a180
Sergei Kutin <skutin-c@dnanexus.com>
2017-12-13 19:14:28 +0300

    same

--- src/app/common/components/Menu/Menu.jsx
+++ src/app/common/components/Menu/Menu.jsx
@@ -8,9 +8,15 @@ export default class Menu extends Component {
         return  <div>
             <h1>My test app</h1>
             <div className="menu">
-                <NavLink exact className="menu__item" activeClassName="active" to="/">{ this.props.appStore.personalDataForm.title }</NavLink>>
-                <NavLink className="menu__item" activeClassName="active" to="/paydata">{ this.props.appStore.payDataForm.title }</NavLink>>
-                <NavLink className="menu__item" activeClassName="active" to="/submit">{ this.props.appStore.finishForm.title }</NavLink>
+                <NavLink exact className="menu__item" activeClassName="active" to="/">
+                    { this.props.appStore.personalDataForm.title }
+                </NavLink>>
+                <NavLink className="menu__item" activeClassName="active" to="/paydata">
+                    { this.props.appStore.payDataForm.title }
+                </NavLink>>
+                <NavLink className="menu__item" activeClassName="active" to="/submit">
+                    { this.props.appStore.finishForm.title }
+                </NavLink>
             </div>
         </div>
     }
